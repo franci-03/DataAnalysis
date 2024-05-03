1bfc4aec68b10c90dc1cc5d7f2d76ffc2d3c1a68
Francesco <francesco.simonetta3@gmx.ch>
2024-05-03 15:38:57 +0200

    Update Model FIt

---
+++ 03_Validation/validation_final.R
@@ -0,0 +1,234 @@
+### CFA VALIDATION OF CONSTRUCTS FOR COMPLETE DATASET (LAB AND PROLIFIC)
+## v2_19.04.2024
+
+
+setwd("C:/Users/franc/Desktop/DataAnalysis/03_Validation")
+rm(list=ls())
+
+# Load necessary libraries
+library(lavaan)
+library(semTools)
+library(psych)
+library(nortest)
+
+# Load data
+data <- read.csv("data_merged_all_validation.csv")
+
+
+## Preliminary tests
+
+# Exclude non-numeric columns
+numeric_data <- data[sapply(data, is.numeric)]  
+
+# Assessing normality for numeric columns
+normality_tests <- lapply(numeric_data, function(x) shapiro.test(x[1:5000]))
+print(normality_tests)
+
+# -> Because some items are not normal and could bias parameters in CFA, we use a robust estimation (MLR)
+
+# Calculate Correlation Matrix for each construct class
+ailit_vars <- c("AILiteracyUse1", "AILiteracyUse2", "AILiteracyUse3", "AILiteracyUse4", "AILiteracyUse5", "AILiteracyUse6",
+                 "AILiteracyKno1", "AILiteracyKno2", "AILiteracyKno3", "AILiteracyKno4", "AILiteracyKno5", "AILiteracyKno6")
+ailit_data <- data[, ailit_vars]
+cor_matrix_ailit <- cor(ailit_data)
+
+gaai_vars <- c("PGATPGAT1", "PGATPGAT2", "PGATPGAT3",
+               "NGATNGAT1", "NGATNGAT2", "NGATNGAT3")
+gaai_data <- data[, gaai_vars]
+cor_matrix_gaai <- cor(gaai_data)
+
+neo_vars <- c("NEOE1R", "NEOE3R", "NEOE4",
+              "NEOV1R", "NEOV2", "NEOV4R",
+              "NEOG1", "NEOG2R", "NEOG3", "NEOG4",
+              "NEON1", "NEON2R", "NEON3", "NEON4",
+              "NEOO1", "NEOO2", "NEOO3", "NEOO4", "NEOO5R")
+neo_data <- data[, neo_vars]
+cor_matrix_neo <- cor(neo_data)
+
+jc_vars <- c("JC1IStR1", "JC1IStR2", "JC1IStR3", "JC1IStR4", "JC1IStR5",
+             "JC1HRJD1", "JC1HRJD2", "JC1HRJD3",
+             "JC22IStR1", "JC22IStR2", "JC22IStR3", "JC22IStR4", "JC22IStR5",
+             "JC22HRJD1", "JC22HRJD2", "JC22HRJD5", "JC22HRJD6")
+jc_data <- data[, jc_vars]
+cor_matrix_jc <- cor(jc_data)
+
+# Calculate the Kaiser-Meyer-Olkin Measure of Sampling Adequacy
+kmo_results1 <- KMO(ailit_data)
+print(kmo_results1)
+
+kmo_results2 <- KMO(gaai_data)
+print(kmo_results2)
+
+kmo_results3 <- KMO(neo_data)
+print(kmo_results3)
+
+kmo_results4 <- KMO(jc_data)
+print(kmo_results4)
+
+#Average of those 4 as overall Sampling Adequacy = 0.7
+
+# Perform Bartlett’s Test of Sphericity
+all_vars <- c("AILiteracyUse1", "AILiteracyUse2", "AILiteracyUse3", "AILiteracyUse4", "AILiteracyUse5", "AILiteracyUse6",
+                 "AILiteracyKno1", "AILiteracyKno2", "AILiteracyKno3", "AILiteracyKno4", "AILiteracyKno5", "AILiteracyKno6",
+                 "PGATPGAT1", "PGATPGAT2", "PGATPGAT3",
+                 "NGATNGAT1", "NGATNGAT2", "NGATNGAT3",
+                 "NEOE1R", "NEOE3R", "NEOE4",
+                 "NEOV1R", "NEOV2", "NEOV4R",
+                 "NEOG1", "NEOG2R", "NEOG3", "NEOG4",
+                 "NEON1", "NEON2R", "NEON3", "NEON4",
+                 "NEOO1", "NEOO2", "NEOO3", "NEOO4", "NEOO5R",
+                 "JC1IStR1", "JC1IStR2", "JC1IStR3", "JC1IStR4", "JC1IStR5",
+                 "JC1HRJD1", "JC1HRJD2", "JC1HRJD3",
+                 "JC22IStR1", "JC22IStR2", "JC22IStR3", "JC22IStR4", "JC22IStR5",
+                 "JC22HRJD1", "JC22HRJD2", "JC22HRJD5", "JC22HRJD6")
+all_data <- data[, all_vars]
+cor_matrix_all <- cor(all_data)
+
+bartlett_results <- cortest.bartlett(cor_matrix_all, n = 68)
+print(bartlett_results)
+
+
+## CFA
+
+# Confirmatory Factor Analysis Model Specification AI Literacy 
+# AILiteracy_Det and AILiteracy_Eth completely removed
+cfa_model_ailit <- '
+AILiteracy_Use =~ AILiteracyUse1 + AILiteracyUse2 + AILiteracyUse3 + AILiteracyUse4 + AILiteracyUse5 + AILiteracyUse6
+AILiteracy_Kno =~ AILiteracyKno1 + AILiteracyKno2 + AILiteracyKno3 + AILiteracyKno4 + AILiteracyKno5 + AILiteracyKno6
+'
+
+# Fit the CFA model and look at summary
+cfa_fit1 <- cfa(model = cfa_model_ailit, data = data, estimator = "MLR")
+summary(cfa_fit1, fit.measures = TRUE, standardized = TRUE)
+
+# Cronbach's Alpha for each construct
+psych::alpha(data[, c("AILiteracyUse1", "AILiteracyUse2", "AILiteracyUse3", 
+                      "AILiteracyUse4", "AILiteracyUse5", "AILiteracyUse6")], check.keys=TRUE)
+
+psych::alpha(data[, c("AILiteracyKno1", "AILiteracyKno2", "AILiteracyKno3", 
+                      "AILiteracyKno4", "AILiteracyKno5", "AILiteracyKno6")], check.keys=TRUE)
+
+# Composite Reliabilities
+compRelSEM(cfa_fit1)
+
+# Average Variance Extracted:
+AVE(cfa_fit1)
+
+# Discriminant Validity to calculate AVE and apply the Fornell-Larcker criterion
+discriminantValidity(cfa_fit1, cutoff = 0.9, merge = FALSE, level = 0.95)
+
+
+# Confirmatory Factor Analysis Model Specification GAAI
+cfa_model_gaai <- '
+P_GAT =~ PGATPGAT1 + PGATPGAT2 + PGATPGAT3
+N_GAT =~ NGATNGAT1 + NGATNGAT2 + NGATNGAT3
+'
+
+# Fit the CFA model and look at summary
+cfa_fit2 <- cfa(model = cfa_model_gaai, data = data, estimator = "MLR")
+summary(cfa_fit2, fit.measures = TRUE, standardized = TRUE)
+
+# Cronbach's Alpha for each construct
+psych::alpha(data[, c("PGATPGAT1",  "PGATPGAT2", "PGATPGAT3")], check.keys=TRUE)
+
+psych::alpha(data[, c("NGATNGAT1", "NGATNGAT2", "NGATNGAT3")], check.keys=TRUE)
+
+# Composite Reliabilities
+compRelSEM(cfa_fit2)
+
+# Average Variance Extracted:
+AVE(cfa_fit2)
+
+# Discriminant Validity to calculate AVE and apply the Fornell-Larcker criterion
+discriminantValidity(cfa_fit2, cutoff = 0.9, merge = FALSE, level = 0.95)
+
+  
+# Confirmatory Factor Analysis Model Specification Big Five 
+# Removed NEOV3R = Ich kann mich kalt und distanziert verhalten and NEOE2 = Ich bin begeisterungsfähig und kann andere leicht mitreißen -> Not the biggest influence for this type of study
+cfa_model_neo <- '
+NEO_E =~ NEOE1R + NEOE3R + NEOE4
+NEO_A =~ NEOV1R + NEOV2 + NEOV4R
+NEO_C =~ NEOG1 + NEOG2R + NEOG3 + NEOG4 
+NEO_N =~ NEON1 + NEON2R + NEON3 + NEON4
+NEO_O =~ NEOO1 + NEOO2 + NEOO3 + NEOO4
+'
+
+# Fit the CFA model and look at summary
+cfa_fit3 <- cfa(model = cfa_model_neo, data = data, estimator = "MLR")
+summary(cfa_fit3, fit.measures = TRUE, standardized = TRUE)
+
+#cfa_fit3 <- cfa(model = cfa_model_neo, data = data, std.lv=TRUE, estimator = "ML", se = "bootstrap", bootstrap = 1000)
+#cfa_fit3 <- cfa(model = cfa_model_neo, data = data, ordered = names(data), estimator = "WLSMV")
+
+# Cronbach's Alpha for each construct
+psych::alpha(data[, c("NEOE1R", "NEOE3R", "NEOE4")], check.keys=TRUE)
+
+psych::alpha(data[, c("NEOV1R", "NEOV2", "NEOV3R", "NEOV4R")], check.keys=TRUE)
+
+psych::alpha(data[, c("NEOG1", "NEOG2R", "NEOG3", "NEOG4")], check.keys=TRUE)
+
+psych::alpha(data[, c("NEON1", "NEON2R", "NEON3", "NEON4")], check.keys=TRUE)
+
+psych::alpha(data[, c("NEOO1", "NEOO2", "NEOO3", "NEOO4", "NEOO5R")], check.keys=TRUE)
+
+# Composite Reliabilities
+compRelSEM(cfa_fit3, omit.indicators = c("NEOE2", "NEOE4")) #negative loading -> Cronbach's Alpha better as it handles this
+
+# Average Variance Extracted:
+AVE(cfa_fit3)
+
+# Discriminant Validity to calculate AVE and apply the Fornell-Larcker criterion
+discriminantValidity(cfa_fit3, cutoff = 0.9, merge = FALSE, level = 0.95)
+
+
+# Confirmatory Factor Analysis Model Specification JC1
+cfa_model_jc1 <- '
+JC1_IStR =~ JC1IStR1 + JC1IStR2 + JC1IStR3 + JC1IStR4 + JC1IStR5
+JC1_HRJD =~ JC1HRJD1 + JC1HRJD2 + JC1HRJD3
+'
+
+# Fit the CFA model and look at summary
+cfa_fit4 <- cfa(model = cfa_model_jc1, data = data, estimator = "MLR")
+summary(cfa_fit4, fit.measures = TRUE, standardized = TRUE)
+
+# Cronbach's Alpha for each construct
+psych::alpha(data[, c("JC1IStR1", "JC1IStR2", "JC1IStR3", "JC1IStR4", "JC1IStR5")], check.keys=TRUE)
+
+psych::alpha(data[, c("JC1HRJD1", "JC1HRJD2", "JC1HRJD3")], check.keys=TRUE)
+
+# Composite Reliabilities
+compRelSEM(cfa_fit4)
+
+# Average Variance Extracted:
+AVE(cfa_fit4)
+
+# Discriminant Validity to calculate AVE and apply the Fornell-Larcker criterion
+discriminantValidity(cfa_fit4, cutoff = 0.9, merge = FALSE, level = 0.95)
+
+
+# Confirmatory Factor Analysis Model Specification JC2
+# remove JC22HRJD6
+cfa_model_jc2 <- '
+JC2_IStR =~ JC22IStR1 + JC22IStR2 + JC22IStR3 + JC22IStR4 + JC22IStR5
+JC2_HRJD =~ JC22HRJD1 + JC22HRJD2 + JC22HRJD5 
+'
+
+# Fit the CFA model and look at summary
+cfa_fit5 <- cfa(model = cfa_model_jc2, data = data, estimator = "MLR")
+summary(cfa_fit5, fit.measures = TRUE, standardized = TRUE)
+
+# Cronbach's Alpha for each construct
+psych::alpha(data[, c("JC22IStR1", "JC22IStR2", "JC22IStR3", "JC22IStR4", "JC22IStR5")], check.keys=TRUE)
+
+psych::alpha(data[, c("JC22HRJD1", "JC22HRJD2", "JC22HRJD5")], check.keys=TRUE) 
+
+# Composite Reliabilities
+compRelSEM(cfa_fit5)
+
+# Average Variance Extracted:
+AVE(cfa_fit5)
+
+# Discriminant Validity to calculate AVE and apply the Fornell-Larcker criterion
+discriminantValidity(cfa_fit5, cutoff = 0.9, merge = FALSE, level = 0.95)
+
+
--- 04_Analysis/hypo-analysis_final.R
+++ 04_Analysis/hypo-analysis_final.R
@@ -15,6 +15,7 @@ library(lmtest)
 library(lavaan)
 library(semTools)
 library(semPlot)
+library(semptools)
 library(ggplot2)
 library(WebPower)
 
@@ -36,17 +37,7 @@ summary(data[, c("JC1IStR", "JC1HRJD", "JC2IStR", "JC2HRJD")])
 boxplot(data$JC1IStR, data$JC2IStR, names = c("Control", "Experimental"), main = "Job Crafting Scores IStR")
 boxplot(data$JC1HRJD, data$JC2HRJD, names = c("Control", "Experimental"), main = "Job Crafting Scores HRJD")
 
-
-# Calculate the differences in Job Crafting between the conditions
-#data <- data %>%
-#  mutate(Diff_JC_IStR = JC2IStR - JC1IStR,
-#         Diff_JC_HRJD = JC2HRJD - JC1HRJD)
-
 # Shapiro-Wilk test for normality
-# On Differences
-#shapiro.test(data$Diff_JC_IStR) # -> Data has normal distribution
-#shapiro.test(data$Diff_JC_HRJD) # -> Data has normal distribution
-# On each construct
 shapiro.test(data$JC1IStR) # -> Data has normal distribution
 shapiro.test(data$JC2IStR) # -> Data has normal distribution
 shapiro.test(data$JC1HRJD) # -> Data has NOT normal distribution
@@ -103,16 +94,7 @@ pairs(data[, c("SDT1AUT", "SDT1COM", "SDT2AUT", "SDT2COM", "JC1IStR", "JC2IStR")
 boxplot(data$SDT1AUT, data$SDT2AUT,  names = c("AUT1", "AUT2"), main = "Autonomy Needs Level Scores")
 boxplot(data$SDT1COM, data$SDT2COM, names = c("COM1", "COM2"), main = "Competence Needs Level Scores")
 
-# Calculate the differences in SDT between the conditions
-#data <- data %>% mutate(
-#  Diff_AUT = SDT2AUT - SDT1AUT,
-#  Diff_COM = SDT2COM - SDT1COM)
-
 # Shapiro-Wilk test for normality
-# On Differences
-#shapiro.test(data$Diff_AUT) # -> Data has NOT normal distribution
-#shapiro.test(data$Diff_COM) # -> Data has NOT normal distribution
-# On each construct
 shapiro.test(data$SDT1AUT) # -> Data has NOT normal distribution
 shapiro.test(data$SDT2AUT) # -> Data has NOT normal distribution
 shapiro.test(data$SDT1COM) # -> Data has NOT normal distribution
@@ -139,99 +121,17 @@ print(w_test_AUT)
 w_test_COM <- wilcox.test(data$SDT2COM, data$SDT1COM, paired = TRUE, alternative = "two.sided")
 print(w_test_COM)
 
-# # Regression analysis
-# linear_model_AUT <- lm(Diff_JC_IStR ~ Diff_AUT + Diff_COM, data = data)
-# summary(linear_model_AUT)
-# linear_model_COM <- lm(Diff_JC_HRJD ~ Diff_AUT + Diff_COM, data = data)
-# summary(linear_model_COM)
-        
 
 ## Hypo 3a-e
 
 # Visual Analysis
 pairs(data[, c("NEOE", "NEOA", "NEOC", "NEON", "NEOO", "JC2IStR", "JC2HRJD")], upper.panel=panel.smooth)
 
-# # Normality test on the predicted values
-# data$predicted_JC2IStR <- lm(JC2IStR ~ NEOE + NEOA + NEOC + NEON + NEOO, data = data)$fitted.values
-# shapiro.test(data$predicted_JC2IStR)
-# 
-# data$predicted_JC2HRJD <- lm(JC2HRJD ~ NEOE + NEOA + NEOC + NEON + NEOO, data = data)$fitted.values
-# shapiro.test(data$predicted_JC2HRJD)
-# 
-# # VIF to check multicollinearity
-# vif(lm(JC2IStR ~ NEOE + NEOA + NEOC + NEON + NEOO, data = data))
-# vif(lm(JC2HRJD ~ NEOE + NEOA + NEOC + NEON + NEOO, data = data))
 
 ## SEM Moderation IStR
 
-# 1) Simple model without any transformation
 
-# Create interaction terms manually
-data$JC2IStR_NEOO <- data$JC2IStR * data$NEOO
-data$JC2IStR_NEOC <- data$JC2IStR * data$NEOC
-data$JC2IStR_NEOA <- data$JC2IStR * data$NEOA
-data$JC2IStR_NEOE <- data$JC2IStR * data$NEOE
-data$JC2IStR_NEON <- data$JC2IStR * data$NEON
-
-# Model
-model1 <- ' 
-  # Measurement Model Definitions
-  JobCraftingControl =~ JC1IStR
-  JobCraftingExp =~ JC2IStR
-  
-  # Direct Effects
-  JobCraftingExp ~ JobCraftingControl + Prompt_Support + NEOE + NEOA + NEOC + NEON + NEOO
-  
-  # Moderation Effects
-  JobCraftingExp ~ JC2IStR_NEOO + JC2IStR_NEOC + JC2IStR_NEOA + JC2IStR_NEOE + JC2IStR_NEON 
-  
-  # Control Variables
-  JobCraftingExp ~ AGE + WORK + Gender_Männlich + 
-                   Edu_Abgeschlossene_Berufsausbildung + Edu_Mittlere_Reife_Realschulabschluss +
-                   Edu_Abitur_oder_Fachabitur + Edu_Bachelor + Edu_Master + 
-                   AILiteracyUse + AILiteracyKno + AILiteracyDet + AILiteracyEth +
-                   PGAT + NGAT
-
-'
-
-# Fit the model
-fit1 <- sem(model1, data = data, estimator = "MLR") #fixed.x = FALSE #std.lv= TRUE
-summary(fit1, fit.measures = TRUE, standardized = TRUE)
-
-
-# 2) Model looking at JC-Delta
-
-# Create JC-Delta
-data$JC_Difference <- data$JC2IStR - data$JC1IStR
-
-# Create interaction terms manually
-data$JC2IStR_Diff_NEOO <- data$JC_Difference * data$NEOO
-data$JC2IStR_Diff_NEOC <- data$JC_Difference * data$NEOC
-data$JC2IStR_Diff_NEOA <- data$JC_Difference * data$NEOA
-data$JC2IStR_Diff_NEOE <- data$JC_Difference * data$NEOE
-data$JC2IStR_Diff_NEON <- data$JC_Difference * data$NEON
-
-# Model
-model2 <- '
-  JC_Difference ~ Prompt_Support + NEOE + NEOA + NEOC + NEON + NEOO
-  
-  JC_Difference ~ JC2IStR_Diff_NEOO + JC2IStR_Diff_NEOC + JC2IStR_Diff_NEOA + JC2IStR_Diff_NEOE + JC2IStR_Diff_NEON
-  
-  # Control variables
-  JC_Difference ~ AGE + WORK + Gender_Männlich +
-                  Edu_Abgeschlossene_Berufsausbildung + Edu_Mittlere_Reife_Realschulabschluss +
-                  Edu_Abitur_oder_Fachabitur + Edu_Bachelor + Edu_Master +
-                  AILiteracyUse + AILiteracyKno + AILiteracyDet + AILiteracyEth +
-                  PGAT + NGAT
-                  
-'
-
-# Fit the model
-fit2 <- sem(model2, data = data, estimator = "MLR")
-summary(fit2, standardized = TRUE, fit.measures = TRUE)
-
-
-# 3) Model with Double Mean Centering (Handling multicollinearity appropriately) -> YT-Video
+# Model with Double Mean Centering (Handling multicollinearity appropriately) -> YT-Video
 
 # Test model fit without interaction first
 model_no_int <- '
@@ -255,13 +155,19 @@ model_no_int <- '
 fit_no_int <- sem(model_no_int, data = data, estimator = "MLR")
 summary(fit_no_int, fit.measures = TRUE, standardized = TRUE)
 
-# Double Mean Centering the relevant variables
+
+# Double Mean Centering the relevant variables to create interaction terms of Big Five with JC2
 data_dmc <- indProd(data, var1 = c("JC2IStR"),
                    var2 = c("NEOE", "NEOA", "NEOC", "NEON", "NEOO"),
                    match = FALSE, meanC = TRUE,
                    residualC = FALSE, doubleMC = TRUE)
 
+# Check dataset for interaction terms
 head(data_dmc)
+
+# Transform AGE (and WORK) to address non-linear relationship
+data_dmc$AGEsq <- data_dmc$AGE^2
+#data_dmc$WORKsq <- data_dmc$WORK^2
 
 # Moderation model
 model_inter <- '
@@ -276,37 +182,32 @@ model_inter <- '
   JobCraftingExp ~ JC2IStR.NEOE + JC2IStR.NEOA + JC2IStR.NEOC + JC2IStR.NEON + JC2IStR.NEOO
 
   # Control Variables
-  JobCraftingExp ~ AGE + WORK + Gender_Männlich +
+  JobCraftingExp ~ AGE + AGEsq + WORK + Gender_Männlich +
                    Edu_Abgeschlossene_Berufsausbildung + Edu_Mittlere_Reife_Realschulabschluss + 
                    Edu_Abitur_oder_Fachabitur + Edu_Bachelor + Edu_Master + 
-                   AILiteracyUse + AILiteracyKno + AILiteracyDet + AILiteracyEth +
-                   PGAT + NGAT
-
-  # Error Covariances (according to YT Video bc of Interaction Terms as JC2IStR is included everywhere) -> Discuss with Eva
-  # JC2IStR.NEOE ~~ JC2IStR.NEOA + JC2IStR.NEOC + JC2IStR.NEON + JC2IStR.NEOO
-  # JC2IStR.NEOA ~~ JC2IStR.NEOC + JC2IStR.NEON + JC2IStR.NEOO
-  # JC2IStR.NEOC ~~ JC2IStR.NEON + JC2IStR.NEOO
-  # JC2IStR.NEON ~~ JC2IStR.NEOO
+                   AILiteracyUse + AILiteracyKno + PGAT + NGAT
   
 '
 
 # Fit and summarize the model
-fit_inter <- sem(model_inter, data = data_dmc, estimator = "MLR")
+fit_inter <- sem(model_inter, data = data_dmc, estimator = "MLR") #fixed.x = FALSE #std.lv= TRUE
 summary(fit_inter, fit.measures = TRUE, standardized = TRUE)
+
+# Inspect Measures
 fitMeasures(fit_inter, c("cfi", "tli", "rmsea", "srmr"))
 lavInspect(fit_inter, "residuals")
 
-# Bootstrapping
-fit_inter <- sem(model_inter, data = data_dmc, se = "boot", bootstrap = 5000)
-summary(fit_inter, fit.measures = TRUE, standardized = TRUE, ci = TRUE)
-
-# Modification indices (to refine the model) -> To be discussed with Eva
+# Modification indices (to refine the model)
 varTable(fit_inter)
 mod_indices <- modificationIndices(fit_inter)
 head(mod_indices[order(-mod_indices$mi),])
 
+# Alternativ: Bootstrapping
+# fit_inter <- sem(model_inter, data = data_dmc, se = "boot", bootstrap = 5000)
+# summary(fit_inter, fit.measures = TRUE, standardized = TRUE, ci = TRUE)
 
-## Post-hoc power analysis
+
+# Post-hoc power analysis
 
 # Perform power analysis
 power_result <- wp.sem.rmsea(
@@ -323,7 +224,7 @@ power_result <- wp.sem.rmsea(
 print(power_result)
 
 
-## Probing Interactions
+# Probing Interactions
 
 # Calculate mean and standard deviation for NEOE
 mean_NEOA <- mean(data_dmc$NEOA, na.rm = TRUE)
@@ -361,7 +262,7 @@ abline(h = 0, lty = 2)
 text(c(-1, 0, 1), slopes, labels = c("NEOA -1 SD", "NEOA Mean", "NEOA +1 SD"), pos = 3)
 
 
-## Simple sloaps for significant interaction terms
+# Simple sloaps for significant interaction terms
 
 # NOEA
 # Define moderator for probing:
@@ -412,11 +313,7 @@ plotProbe(probeNEOO, c(-3,3))
 
 
 
-## SemPaths -> Difficult to make good graphs (probably use PPT)
-
-library(semPlot)
-library(semptools)
-
+# SemPaths -> Difficult to make good graphs (probably use PPT)
 pm_no_ctrl <- semptools::drop_nodes(
                 object = semPlotModel(fit_inter),
                 nodes = c("AGE", "WORK", "Gender_Männlich", 
@@ -444,74 +341,7 @@ semPaths(pm_no_ctrl, what = "est", layout = "tree",
 
 ## SEM Moderation HRJD
 
-# 1) Simple model without any transformation
-
-# Create interaction terms manually
-data$JC2HRJD_NEOO <- data$JC2HRJD * data$NEOO
-data$JC2HRJD_NEOC <- data$JC2HRJD * data$NEOC
-data$JC2HRJD_NEOA <- data$JC2HRJD * data$NEOA
-data$JC2HRJD_NEOE <- data$JC2HRJD * data$NEOE
-data$JC2HRJD_NEON <- data$JC2HRJD * data$NEON
-
-# Model
-model1 <- ' 
-  # Measurement Model Definitions
-  JobCraftingControl =~ JC1HRJD
-  JobCraftingExp =~ JC2HRJD
-  
-  # Direct Effects
-  JobCraftingExp ~ JobCraftingControl + Prompt_Support + NEOE + NEOA + NEOC + NEON + NEOO
-  
-  # Moderation Effects
-  JobCraftingExp ~ JC2HRJD_NEOO + JC2HRJD_NEOC + JC2HRJD_NEOA + JC2HRJD_NEOE + JC2HRJD_NEON 
-  
-  # Control Variables
-  JobCraftingExp ~ AGE + WORK + Gender_Männlich + 
-                   Edu_Abgeschlossene_Berufsausbildung + Edu_Mittlere_Reife_Realschulabschluss +
-                   Edu_Abitur_oder_Fachabitur + Edu_Bachelor + Edu_Master + 
-                   AILiteracyUse + AILiteracyKno + AILiteracyDet + AILiteracyEth +
-                   PGAT + NGAT
-                   
-'
-
-# Fit the model
-fit1 <- sem(model1, data = data, estimator = "MLR") #fixed.x = FALSE #std.lv= TRUE
-summary(fit1, fit.measures = TRUE, standardized = TRUE)
-
-
-# 2) Model looking at JC-Delta
-
-# Create JC-Delta
-data$JC_Difference <- data$JC2HRJD - data$JC1HRJD
-
-# Create interaction terms manually
-data$JC2HRJD_Diff_NEOO <- data$JC_Difference * data$NEOO
-data$JC2HRJD_Diff_NEOC <- data$JC_Difference * data$NEOC
-data$JC2HRJD_Diff_NEOA <- data$JC_Difference * data$NEOA
-data$JC2HRJD_Diff_NEOE <- data$JC_Difference * data$NEOE
-data$JC2HRJD_Diff_NEON <- data$JC_Difference * data$NEON
-
-# Model
-model2 <- '
-  JC_Difference ~ Prompt_Support + NEOE + NEOA + NEOC + NEON + NEOO
-  
-  JC_Difference ~ JC2HRJD_Diff_NEOO + JC2HRJD_Diff_NEOC + JC2HRJD_Diff_NEOA + JC2HRJD_Diff_NEOE + JC2HRJD_Diff_NEON
-  
-  # Control variables
-  JC_Difference ~ AGE + WORK + Gender_Männlich +
-                  Edu_Abgeschlossene_Berufsausbildung + Edu_Mittlere_Reife_Realschulabschluss +
-                  Edu_Abitur_oder_Fachabitur + Edu_Bachelor + Edu_Master +
-                  AILiteracyUse + AILiteracyKno + AILiteracyDet + AILiteracyEth +
-                  PGAT + NGAT
-                  
-'
-
-# Fit the model
-fit2 <- sem(model2, data = data, estimator = "MLR")
-summary(fit2, standardized = TRUE, fit.measures = TRUE)
-
-
-# 3) Model with Double Mean Centering
+# Model with Double Mean Centering
 
 # Test model fit without interaction first
 model_no_int <- '
@@ -535,12 +365,19 @@ model_no_int <- '
 fit_no_int <- sem(model_no_int, data = data, estimator = "MLR")
 summary(fit_no_int, fit.measures = TRUE, standardized = TRUE)
 
-# Double Mean Centering the relevant variables
+
+# Double Mean Centering the relevant variables to create interaction terms of Big Five with JC2
 data_dmc <- indProd(data, var1 = c("JC2HRJD"),
                      var2 = c("NEOE", "NEOA", "NEOC", "NEON", "NEOO"),
                      match = FALSE, meanC = TRUE,
                      residualC = FALSE, doubleMC = TRUE)
+
+# Check dataset for interaction terms
 head(data_dmc)
+
+# Transform AGE (and WORK) to address non-linear relationship
+data_dmc$AGEsq <- data_dmc$AGE^2
+data_dmc$WORKsq <- data_dmc$WORK^2
 
 # Moderation model
 model_inter <- '
@@ -555,37 +392,33 @@ model_inter <- '
   JobCraftingExp ~ JC2HRJD.NEOE + JC2HRJD.NEOA + JC2HRJD.NEOC + JC2HRJD.NEON + JC2HRJD.NEOO
 
   # Control Variables
-  JobCraftingExp ~ AGE + WORK + Gender_Männlich +
+  JobCraftingExp ~ AGE + AGEsq + WORK + Gender_Männlich +
                    Edu_Abgeschlossene_Berufsausbildung + Edu_Mittlere_Reife_Realschulabschluss + 
                    Edu_Abitur_oder_Fachabitur + Edu_Bachelor + Edu_Master + 
-                   AILiteracyUse + AILiteracyKno + AILiteracyDet + AILiteracyEth +
-                   PGAT + NGAT
-
-  # Error Covariances (according to YT Video bc of Interaction Terms as JC2HRJD is included everywhere) -> Discuss with Eva
-  # JC2HRJD.NEOE ~~ JC2HRJD.NEOA + JC2HRJD.NEOC + JC2HRJD.NEON + JC2HRJD.NEOO
-  # JC2HRJD.NEOA ~~ JC2HRJD.NEOC + JC2HRJD.NEON + JC2HRJD.NEOO
-  # JC2HRJD.NEOC ~~ JC2HRJD.NEON + JC2HRJD.NEOO
-  # JC2HRJD.NEON ~~ JC2HRJD.NEOO
+                   AILiteracyUse + AILiteracyKno + PGAT + NGAT
   
 '
 
 # Fit and summarize the model
 fit_inter <- sem(model_inter, data = data_dmc, estimator = "MLR")
 summary(fit_inter, fit.measures = TRUE, standardized = TRUE)
+
+# Inspect Measures
 fitMeasures(fit_inter, c("cfi", "tli", "rmsea", "srmr"))
 lavInspect(fit_inter, "residuals")
-
-# Bootstrapping
-fit_inter <- sem(model_inter, data = data_dmc, se = "boot", bootstrap = 5000)
-summary(fit_inter, fit.measures = TRUE, standardized = TRUE, ci = TRUE)
 
 # Modification indices (to refine the model) -> To be discussed with Eva
 varTable(fit_inter)
 mod_indices <- modificationIndices(fit_inter)
 head(mod_indices[order(-mod_indices$mi),])
 
+# Alternative - Bootstrapping
+fit_inter <- sem(model_inter, data = data_dmc, se = "boot", bootstrap = 5000)
+summary(fit_inter, fit.measures = TRUE, standardized = TRUE, ci = TRUE)
 
-## Post-hoc power analysis
+
+
+# Post-hoc power analysis
 
 # Perform power analysis
 power_result <- wp.sem.rmsea(
@@ -604,54 +437,3 @@ print(power_result)
 
 
 
-
-
-
-### Improve model fit
-
-
-# Standardizing AGE and WORK in the dataset
-data_dmc$AGE_std <- scale(data_dmc$AGE, center = TRUE, scale = TRUE)
-data_dmc$WORK_std <- scale(data_dmc$WORK, center = TRUE, scale = TRUE)
-
-# Adding squared term for standardized AGE
-data_dmc$AGEsq_std <- data_dmc$AGE_std^2
-
-data_dmc$AGEsq <- data_dmc$AGE^2
-data_dmc$WORKsq_std <- data_dmc$WORK_std^2
-
-data_dmc$WORK_log <- log(data_dmc$WORK + 1)
-data_dmc$WORKsq_log <- data_dmc$WORK_log^2
-
-
-# Moderation model
-model_inter <- '
-  # Measurement Model Definitions
-  JobCraftingControl =~ JC1HRJD
-  JobCraftingExp =~ JC2HRJD
-
-  # Direct Effects
-  JobCraftingExp ~ JobCraftingControl + Prompt_Support + NEOE + NEOA + NEOC + NEON + NEOO
-
-  # Moderation Effects
-  JobCraftingExp ~ JC2HRJD.NEOE + JC2HRJD.NEOA + JC2HRJD.NEOC + JC2HRJD.NEON + JC2HRJD.NEOO
-
-  # Control Variables
-  JobCraftingExp ~ AGE + AGEsq + WORK + Gender_Männlich +
-                   Edu_Abgeschlossene_Berufsausbildung + Edu_Mittlere_Reife_Realschulabschluss + 
-                   Edu_Abitur_oder_Fachabitur + Edu_Bachelor + Edu_Master + 
-                   AILiteracyUse + AILiteracyKno + AILiteracyDet + AILiteracyEth +
-                   PGAT + NGAT
-
-'
-
-# Fit and summarize the model
-fit_inter <- sem(model_inter, data = data_dmc, estimator = "MLR")
-summary(fit_inter, fit.measures = TRUE, standardized = TRUE)
-
-lavInspect(fit_inter, "residuals")
-
-# Modification indices (to refine the model) -> To be discussed with Eva
-varTable(fit_inter)
-mod_indices <- modificationIndices(fit_inter)
-head(mod_indices[order(-mod_indices$mi),])
